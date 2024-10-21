f5c7ab8aa7e2b27151fa1694a4ca73ad7912fe1a
Jamieson <158116310+ash-comsci@users.noreply.github.com>
2024-10-20 20:59:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 /**
- * ICS3 Template
-**/
+ * 2.6 - Connecting HTML to JavaScript!
+ * ICS3 - Mr. J
+ * 
+ * Read README.md and YOUR_Task.md 
+ **/ 
+
+// Follow the rules
+'use strict';
+
+/*** Event Listeners ***/
+
+
+/*** Functions ***/
+
+// Return a random integer from `min` to `max`, inclusive
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
